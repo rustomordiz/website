b89c43f0c3c9d56c2ee52eb04d4854b409a13d1c
Rustom Ordiz <rustomanthony81@gmail.com>
2024-09-16 01:56:55 +0800

    light mode and dark mode

--- app.js
+++ app.js
@@ -38,6 +38,13 @@ function PageTransitions(){
             }
         });
     });
+
+    // Toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',() =>{
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
 }
 
 PageTransitions();
